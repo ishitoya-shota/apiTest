5a5cd080ae9f9cf661b31235f1ee0e30038497a3
ishitoya-shota <kstru7818ssk2136@gmail.com>
2025-08-21 00:02:17 +0900

    curlコマンド実行した結果をまとめた

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
-# 簡単なAPIを作って curl を試す方法
+
+git # 簡単なAPIを作って curl を試す方法
 
 ## 1. まずはローカルに簡単なAPIを立てる
 
@@ -27,3 +28,39 @@ def upload():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
 
+# 以下は実際に実行したcurlコマンドの例と出力
+
+# curl -X POST http://127.0.0.1:5000/upload \
+#   -F "loginuser=testuser" \
+#   -F "feature={\"key\":\"id\", \"value\":\"big\"}" \
+#   -F "data=@test.txt"
+
+# 出力
+# {
+#   "feature": "{\"key\":\"id\", \"value\":\"big\"}",
+#   "filename": "test.txt",
+#   "loginuser": "testuser"
+# }
+
+
+# curl -sS -X POST \
+#   -F 'loginuser=testuser' \
+#   --form-string 'feature=[{"key":"id","value":"big"}]' \
+#   -F 'data=@test.txt' \
+#   -i \
+#   -w '\n--> %{http_code} | %{content_type}\n' \
+#   http://127.0.0.1:5000/upload 
+
+# HTTP/1.1 200 OK
+# Server: Werkzeug/3.1.3 Python/3.12.10
+# Date: Wed, 20 Aug 2025 14:55:20 GMT
+# Content-Type: application/json
+# Content-Length: 109
+# Connection: close
+
+# 出力
+# {
+#   "feature": "[{\"key\":\"id\",\"value\":\"big\"}]",
+#   "filename": "test.txt",
+#   "loginuser": "testuser"
+# }
